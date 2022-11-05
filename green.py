f45bb855aa9144b944a72ed5c039221130cfa99e
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-11-05 20:08:56 +0600

    Update green.py

--- green.py
+++ green.py
@@ -1,31 +1,24 @@
-import os, platform
- 
-try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from mahadi import mahadixd
- 
-        mahadixd()
- 
- 
- 
-elif bit == "32bit":
- 
-        from mahadi32 import mahadinx
- 
- 
-        mahadinx()
- 
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="32bit":
+    print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import FL32.MahadiNX
+
+
+elif bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import FL.MahadiNX
+
+else:
+    print('\nUNKNOWN DEVICE')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI
+#---------------------------------------------------------#
