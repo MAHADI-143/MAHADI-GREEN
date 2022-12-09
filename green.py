c9e191be05659d5f08ff67051af81423041a3a2d
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-12-09 14:24:13 +0400

    Update green.py

--- green.py
+++ green.py
@@ -1,30 +1,5 @@
-import os, platform
- 
+import os, sys
 try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from mahadi import mahadi
- 
-        mahadi()
- 
- 
- 
-elif bit == "32bit":
- 
-        from FL32 import MahadiNX
- 
- 
-        MahadiNX()
+    __import__("mahadi").mahadi()
+except Exception as e:
+    exit(str(e))
