5392652df7bb3ac08cf4ec4a60f282bc4cd887a9
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-12-14 18:43:11 +0400

    Update green.py

--- green.py
+++ green.py
@@ -1,5 +1,9 @@
-import os, sys
-try:
-    __import__("mahadii").MahadiNX()
-except Exception as e:
-    exit(str(e))
+import os,time,platform
+os.system('clear')
+print('[>] Checking Updates')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import mahadi
+else:
+    print('\033[1;31m[×] Sorry Device Not Support')
