947dc15646172c55ba5fae1de581b20e9e6c1626
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-03-01 11:49:30 +0400

    Update green.py

--- green.py
+++ green.py
@@ -4,6 +4,6 @@ print('[>] Checking Updates')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import mahadi
+    import FILE
 else:
     print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
