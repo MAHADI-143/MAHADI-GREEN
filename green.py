dd5580cc4766c91e7f000c5e35b3586a5163f717
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-03-14 20:22:15 +0300

    Delete green.py

--- green.py
+++
@@ -1,9 +0,0 @@
-import os,time,platform
-os.system('clear')
-print('[>] Checking Updates')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    import mahadi
-else:
-    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
