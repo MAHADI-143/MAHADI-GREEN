8705369e2b2751f03683dc47c6c2ff0e42609977
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-10-02 11:32:53 +0300

    Update FILE-DUMP.py

--- FILE-DUMP.py
+++ FILE-DUMP.py
@@ -1,10 +1,12 @@
 import os,time,platform
-os.system('clear')
-print('[>] Checking Updates')
-os.system('git pull')
+#os.system('clear')
+#print('[>] Checking Updates')
+#os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import DUMP64
+    import FILE64
+elif bit=='32bit':
+    import FILE32
 else:
-    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
+    print('\033[1;31m[×] Internet Problem')
  
