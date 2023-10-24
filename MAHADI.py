30c5ba1f95bd74587a4bb85fa388752e80337824
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-10-24 08:21:07 +0300

    Update MAHADI.py

--- MAHADI.py
+++ MAHADI.py
@@ -4,7 +4,7 @@ import os,time,platform
 #os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import FILEX
+    import FILX
 else:
     print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
  
