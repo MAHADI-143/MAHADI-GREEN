df8fe0222b27355a5938585b60988f16df2bb78f
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-10-25 11:28:40 +0300

    Update and rename MAHADI.py to GREEN.py

---
+++ GREEN.py
@@ -0,0 +1,8 @@
+import os,time,platform
+Mahadixx = platform.architecture()[0]
+if Mahadixx=='64bit':
+    import FILX
+else:
+    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
+ 
+ 
--- MAHADI.py
+++
@@ -1,1 +0,0 @@
-print('Tool Off')
