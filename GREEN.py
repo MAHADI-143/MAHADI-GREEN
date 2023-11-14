fb86434d86d4ed841049e52f8f61cf02fa56332a
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-11-14 13:33:57 +0600

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,1 +1,8 @@
-print("tool off")
+import os,time,platform
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import FILE
+else:
+    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
+ 
+ 
